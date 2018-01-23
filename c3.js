bbdafe6c8c0fc3e46e9116ac3f9f162067987add
xavier <xavier@imagine-app.fr>
2018-01-23 17:00:24 +0100

    fix missing token

--- c3.js
+++ c3.js
@@ -644,10 +644,9 @@ c3_axis_fn.init = function init() {
     $$.axes.y.append("text").attr("class", CLASS.axisYLabel).attr("transform", config.axis_rotated ? "" : "rotate(-90)").style("text-anchor", this.textAnchorForYAxisLabel.bind(this));
 
     $$.axes.y2 = main.append("g").attr("class", CLASS.axis + ' ' + CLASS.axisY2
-		$$.axes.y3 = main.append("g").attr("class", CLASS.axis + ' ' + CLASS.axisY3
     // clip-path?
-    ).attr("transform", $$.getTranslate('y2')).style("visibility", config.axis_y2_show ? 'visible' : 'hidden')
-		.attr("transform", $$.getTranslate('y3')).style("visibility", config.axis_y3_show ? 'visible' : 'hidden');
+	).attr("transform", $$.getTranslate('y2')).style("visibility", config.axis_y2_show ? 'visible' : 'hidden');
+		$$.axes.y3 = main.append("g").attr("class", CLASS.axis + ' ' + CLASS.axisY3.attr("transform", $$.getTranslate('y3')).style("visibility", config.axis_y3_show ? 'visible' : 'hidden');
     $$.axes.y2.append("text").attr("class", CLASS.axisY2Label).attr("transform", config.axis_rotated ? "" : "rotate(-90)").style("text-anchor", this.textAnchorForY2AxisLabel.bind(this));
 		$$.axes.y3.append("text").attr("class", CLASS.axisY3Label).attr("transform", config.axis_rotated ? "" : "rotate(-90)").style("text-anchor", this.textAnchorForY3AxisLabel.bind(this));
 };
