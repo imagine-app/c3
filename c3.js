66a6e7b075a26a6ca1f16e7c3524f4898a9953f3
xavier <xavier@imagine-app.fr>
2018-01-24 14:40:02 +0100

    remove 100 for translate y3

--- c3.js
+++ c3.js
@@ -1873,8 +1873,8 @@ c3_chart_internal_fn.getTranslate = function (target) {
         x = config.axis_rotated ? 0 : $$.width;
         y = config.axis_rotated ? 1 : 0;
     } else if (target === 'y3') {
-        x = config.axis_rotated ? 0 : $$.width;
-        y = config.axis_rotated ? 0.5 : 0;
+        x = config.axis_rotated ? 0 : ($$.width - 100);
+        y = config.axis_rotated ? 1 : 0;
     } else if (target === 'subx') {
         x = 0;
         y = config.axis_rotated ? 0 : $$.height2;
