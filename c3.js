83ca2e9d3cbb3912d2e964eb71eccb8464e82cd8
xavier <xavier@imagine-app.fr>
2018-01-25 15:44:45 +0100

    test

--- c3.js
+++ c3.js
@@ -1877,9 +1877,9 @@ c3_chart_internal_fn.getTranslate = function (target) {
         y = config.axis_rotated ? 1 : 0;
     } else if (target === 'y3') {
 				add_axis_y2_lenght = 0;
-				if(config.axis_y2_show && config.axis_y3_show) {
-					add_axis_y2_lenght = 40;
-				}
+				// if(config.axis_y2_show && config.axis_y3_show) {
+				// 	add_axis_y2_lenght = 40;
+				// }
         x = config.axis_rotated ? 0 : ($$.width + add_axis_y2_lenght);
         y = config.axis_rotated ? 1 : 0;
     } else if (target === 'subx') {
@@ -8637,7 +8637,7 @@ c3_chart_internal_fn.getCurrentPaddingRight = function () {
         // && !config.axis_rotated
         return 2 + legendWidthOnRight + ($$.axis.getY3AxisLabelPosition().isOuter ? 20 : 0);
     } else {
-        return ceil10($$.getAxisWidthByAxisId('y3')) + legendWidthOnRight + resize_y3;
+        return ceil10($$.getAxisWidthByAxisId('y3')) + legendWidthOnRight; //+ resize_y3;
     }
 };
 
